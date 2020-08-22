b37c000098c7eea56669c920bcde8d9d34563206
Raphael Kabo <raphaelkabo@gmail.com>
2020-08-22 22:43:24 +0100

    Moved some files around

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable no-restricted-syntax */
 require('dotenv').config();
-require("regenerator-runtime/runtime");
+require('regenerator-runtime/runtime');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
-const configDatabase = require('./database').config;
-const { transporter } = require('./mailer');
+const configDatabase = require('./config/database').config;
+const { mailer } = require('./mailer');
 const { authHandler } = require('./helpers/authHandler');
 
 const {
@@ -35,37 +36,30 @@ const {
   leaveCommunity,
 } = require('./modules/community/controller');
 const { createRelationship } = require('./modules/relationship/controller');
-
+const { createImage } = require('./modules/image/controller');
 
 const app = express();
 const port = process.env.PORT || 8787;
 
 // CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  next();
-});
+app.use(cors());
 
 // Nodemailer
-transporter.verify(function(error, success) {
-	if (error) {
-		console.log("Email server error!")
-		console.log(error); 
-	} else {
-		console.log("Email server is ready to take our messages");
-	}
+mailer.verify((error, success) => {
+  if (error) {
+    console.log('Email server error!');
+    console.log(error);
+  } else {
+    console.log('Email server is ready to take our messages');
+  }
 });
 
 mongoose.connect(configDatabase.url, { useNewUrlParser: true });
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/api/*', authHandler)
+app.use('/api/*', authHandler);
 
 app.post('/api/expo_token/register', registerExpoToken);
 app.post('/api/register', register);
@@ -74,8 +68,8 @@ app.post('/api/login', login);
 app.get('/api/posts/:context?/:timestamp?/:identifier?', listPosts);
 app.post('/api/post', createPost);
 app.post('/api/plus/:postid', plusPost);
-app.post('/api/boost/:postid/:locationid?', boostPost)
-app.post('/removeboost/:postid', unboostPost)
+app.post('/api/boost/:postid/:locationid?', boostPost);
+app.post('/removeboost/:postid', unboostPost);
 app.post('/api/comment/:postid/:commentid?', createComment);
 
 app.get('/api/communities/all', listCommunities);
@@ -83,7 +77,7 @@ app.get('/api/communities/:communityid', detailCommunity);
 app.post('/api/community/join', joinCommunity);
 app.post('/api/community/leave', leaveCommunity);
 
-app.get('/api/users/:sortorder', listUsers)
+app.get('/api/users/:sortorder', listUsers);
 app.get('/api/user/:identifier', detailUser);
 app.post('/api/settings', changeSettings);
 app.post('/api/report', reportUser);
@@ -92,6 +86,8 @@ app.post('/api/relationship', createRelationship);
 
 app.get('/api/code-of-conduct', getCoC);
 app.post('/api/code-of-conduct/accept', acceptCoC);
+
+app.post('/api/image', createImage);
 
 app.listen(port);
 
--- src/mailer.js
+++ src/mailer.js
@@ -1,15 +1,15 @@
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-	host: process.env.EMAIL_SERVER,
-	port: 587,
-	secure: false, // upgrade later with STARTTLS
-	auth: {
-		user: process.env.EMAIL_USERNAME,
-		pass: process.env.EMAIL_PASSWORD
-	}
+const mailer = nodemailer.createTransport({
+  host: process.env.EMAIL_SERVER,
+  port: 587,
+  secure: false, // upgrade later with STARTTLS
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 module.exports = {
-  transporter,
+  mailer,
 };
