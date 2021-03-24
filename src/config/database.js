14940a4995bb1cb125ef60eb15101903954cfbac
Raphael Kabo <raphaelkabo@gmail.com>
2021-03-24 00:44:43 +0000

    Removed database.js

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoName = '127.0.0.1:27017';
-
-const config = {
-  url: 'mongodb+srv://admin:Braz3nHe4d@sweet-qpcnn.mongodb.net/sweetdev?retryWrites=true&w=majority'
-};
-
-module.exports = {
-  config,
-};
